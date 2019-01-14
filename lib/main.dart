69766e891622f1e6e36d234d5351681fa02b568f
Derek O'Kelly <derekokelly1997@gmail.com>
2019-01-14 21:42:14 +0000

    add camera package and make main function Future

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:insect_classifier/home_page.dart';
 import 'package:insect_classifier/setuppage.dart';
 import 'package:insect_classifier/splash_screen.dart';
+import 'package:camera/camera.dart';
 
-void main() => runApp(MyApp());
+List<CameraDescription> cameras;
 
-var routes = <String, WidgetBuilder> {
+Future<void> main() async {
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+var routes = <String, WidgetBuilder>{
   "/setup": (BuildContext context) => SetupPage(),
   "/home": (BuildContext context) => HomePage(),
 };
