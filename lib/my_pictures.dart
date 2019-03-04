211e378fb99870cf1e4288563fe52ad123f0a0d8
Derek O'Kelly <derekokelly1997@gmail.com>
2019-03-04 00:42:28 +0000

    move permissions request

--- lib/my_pictures.dart
+++ lib/my_pictures.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 
-class MyPictures extends StatelessWidget {
+class MyPictures extends StatefulWidget {
+
   static File image1 = File("/storage/emulated/0/Pictures/Insect Classifier/image1.jpg");
   static File image2 = File("/storage/emulated/0/Pictures/Insect Classifier/image2.jpg");
-  List<File> photos = [image1, image2];
+
+  @override
+  _MyPicturesState createState() => _MyPicturesState();
+}
+
+class _MyPicturesState extends State<MyPictures> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  List<File> photos = [MyPictures.image1, MyPictures.image2];
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +29,14 @@ class MyPictures extends StatelessWidget {
           child: Column(
             children: <Widget>[
               SizedBox(
-                  height: 200.0,
-                  child: Image.file(
-                    photos[index],
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  )),
+                height: 200.0,
+                child: Image.file(
+                  photos[index],
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+              ),
               ButtonTheme.bar(
                 child: ButtonBar(
                   alignment: MainAxisAlignment.spaceBetween,
