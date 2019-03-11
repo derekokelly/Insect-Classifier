acd3a7cc8e849569564ff0816204b868bb35d50e
Derek O'Kelly <derekokelly1997@gmail.com>
2019-03-11 20:06:13 +0000

    Implement Cloud FireStore to store image download URLs

--- lib/camera_tab.dart
+++ lib/camera_tab.dart
@@ -3,9 +3,11 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:simple_permissions/simple_permissions.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CameraPage extends StatefulWidget {
-  static List uploads = [];
   @override
   _CameraPageState createState() => _CameraPageState();
 }
@@ -17,59 +19,77 @@ class _CameraPageState extends State<CameraPage> {
   StorageReference firebaseStorageRef;
   StorageUploadTask task;
 
+  FirebaseDatabase mDatabase;
+  DatabaseReference databaseReference;
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
   Future getImageFromCamera() async {
-
-    PermissionStatus res = await SimplePermissions.requestPermission(Permission. WriteExternalStorage);
-    print(res);
-
-    // Permission.Camera
-    // Permission.WriteExternalStorage, Permission.ReadExternalStorage
+    PermissionStatus res = await SimplePermissions.requestPermission(
+        Permission.WriteExternalStorage);
 
     if (res == PermissionStatus.authorized) {
       var image = await ImagePicker.pickImage(source: ImageSource.camera);
-      var imageName = image.path.split("/");
-
-      if (image != null) {
-        setState(() {
-        _image = image;
-        });
-
-        var metadata = {
-          'insect': 'spider',
-        };
-
-//        firebaseStorageRef = FirebaseStorage.instance.ref().child("images/" + imageName.last);
-//        task = firebaseStorageRef.putFile(image, StorageMetadata(customMetadata: metadata));
-//        Future future = (await task.onComplete).ref.getDownloadURL();
-//        _downloadUrl = future.toString();
-//        CameraPage.uploads.add(_downloadUrl);
-//        print(getImageUris());
-
-        firebaseStorageRef = FirebaseStorage.instance.ref().child("images/" + imageName.last);
-        task = firebaseStorageRef.putFile(image, StorageMetadata(customMetadata: metadata));
-
-        task.onComplete.then((var test) async {
-          _downloadUrl = await firebaseStorageRef.getDownloadURL();
-          print("DOWNLOAD URL " + _downloadUrl);
-          CameraPage.uploads.add(_downloadUrl);
-          print(getImageUris());
-        });
-
-
-      }
+      uploadImage(image);
     }
   }
 
-  static List getImageUris() {
-    return CameraPage.uploads;
-  }
-
   Future getImageFromGallery() async {
+    PermissionStatus res = await SimplePermissions.requestPermission(
+        Permission.ReadExternalStorage);
+
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    uploadImage(image);
 
     if (image != null) {
       setState(() {
-      _image = image;
+        _image = image;
+      });
+    }
+  }
+
+  void uploadImage(image) {
+    var imageName = timestamp() + ".jpg";
+
+    if (image != null) {
+      setState(() {
+        _image = image;
+      });
+
+      var metadata = {
+        'insect': 'spider',
+      };
+
+      Fluttertoast.showToast(
+          msg: "Uploading image to Firebase...",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          fontSize: 16.0);
+
+      firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("images/" + imageName);
+      task = firebaseStorageRef.putFile(
+          image, StorageMetadata(customMetadata: metadata));
+
+      task.onComplete.then((var test) async {
+        _downloadUrl = await firebaseStorageRef.getDownloadURL();
+
+        final DocumentReference documentReference =
+            Firestore.instance.document("images/$imageName");
+        Map<String, String> data = <String, String>{
+          "downloadUrl": _downloadUrl,
+        };
+
+        documentReference.setData(data).whenComplete(() {
+          print("data added");
+          Fluttertoast.showToast(
+              msg: "Finsished uploading",
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              fontSize: 16.0);
+        }).catchError((e) => print(e));
+
+        print("DOWNLOAD URL " + _downloadUrl);
       });
     }
   }
@@ -81,11 +101,11 @@ class _CameraPageState extends State<CameraPage> {
         ButtonBar(
           children: <Widget>[
             IconButton(
-              icon: Icon (Icons.camera_alt),
+              icon: Icon(Icons.camera_alt),
               onPressed: getImageFromCamera,
             ),
             IconButton(
-              icon: Icon (Icons.photo),
+              icon: Icon(Icons.photo),
               onPressed: getImageFromGallery,
             ),
           ],
@@ -95,4 +115,3 @@ class _CameraPageState extends State<CameraPage> {
     );
   }
 }
-
--- lib/my_pictures.dart
+++ lib/my_pictures.dart
@@ -1,7 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'camera_tab.dart';
 
 class MyPictures extends StatefulWidget {
   @override
@@ -9,59 +8,58 @@ class MyPictures extends StatefulWidget {
 }
 
 class _MyPicturesState extends State<MyPictures> {
-//  static String downloadUrl = "https://firebasestorage.googleapis.com/v0/b/insectclassifier.appspot.com/o/f58a9ade-d3a6-4245-a18a-c624214419e06125892513033482463.jpg?alt=media&token=735b28c1-e196-49a7-b15e-cb62001f36cb";
-
   @override
   void initState() {
     super.initState();
   }
 
-  List photos = CameraPage.uploads;
-
-  static var storage = FirebaseStorage.instance.ref();
-  var httpsReference = storage.child('gs://insectclassifier.appspot.com/images');
+  final CollectionReference collectionReference =
+      Firestore.instance.collection("images/");
 
   // TODO: GET METADATA FROM URL
 
+  Widget _buildListItem(context, index) {
+    return Card(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 200.0,
+            child: CachedNetworkImage(
+              placeholder: LinearProgressIndicator(),
+              imageUrl: index['downloadUrl'],
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          ButtonTheme.bar(
+            child: ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Spider"),
+                FlatButton(
+                  child: Text("More info"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: photos.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Card(
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 200.0,
-//                child: Image.network(
-//                  photos[index],
-//                  fit: BoxFit.cover,
-//                  width: double.infinity,
-//                  height: double.infinity,
-//                ),
-                child: CachedNetworkImage(
-                  placeholder: LinearProgressIndicator(),
-                  imageUrl: photos[index],
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-                ),
-              ),
-              ButtonTheme.bar(
-                child: ButtonBar(
-                  alignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("Spider"),
-                    FlatButton(
-                      child: Text("More info"),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return StreamBuilder(
+      stream: Firestore.instance.collection("images").snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Text("Loading...");
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, index) =>
+              _buildListItem(context, snapshot.data.documents[index]),
         );
       },
     );
